ee814793f2e81afa06743fb2249057efd66556be
Your Name <you@example.com>
2019-05-08 16:22:41 -0500

    DB set up

---
+++ models/characters.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Characters = sequelize.define('Characters', {
+    name: DataTypes.STRING,
+    title: DataTypes.STRING,
+    height: DataTypes.FLOAT,
+    weight: DataTypes.INTEGER,
+    superPower: DataTypes.STRING,
+    skillOne: DataTypes.STRING,
+    skillTwo: DataTypes.STRING,
+    enhancedAbilityOne: DataTypes.STRING,
+    enhancedAbilityTwo: DataTypes.STRING,
+    enhancedAbilityThree: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+    childhood: DataTypes.STRING,
+    training: DataTypes.STRING,
+    career: DataTypes.STRING,
+    accomplishments: DataTypes.STRING,
+    family: DataTypes.STRING
+  }, {});
+  Characters.associate = function(models) {
+    // associations can be defined here
+  };
+  return Characters;
+};
